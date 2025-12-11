e95ce8db741636de26cc0719d319a35f32841072
etud user <etud@port-hp-probook650G8-15>
2025-12-11 13:13:53 +0100

    Refonte complète: Page Contact moderne + Header style Badminteam + Nettoyage CSS

--- assets/app.js
+++ assets/app.js
@@ -7,7 +7,7 @@ import 'bootstrap';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
