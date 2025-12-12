62af3b26ba2046f7725e76d41f744085bdf84c13
flavien.leguerneve@gmail.com <etud@port-hp-probook650G8-5>
2025-12-12 11:14:35 +0100

    AJout de la plage galerie

---
+++ src/Controller/AlbumController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Album;
+use App\Entity\CategorieAlbum;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AlbumController extends AbstractController
+{
+    #[Route('/album/{idAlbum}', name: 'album_show')]
+    public function show(int $idAlbum, ManagerRegistry $doctrine): Response
+    {
+        $albumRepo = $doctrine->getRepository(Album::class);
+        $categorieRepo = $doctrine->getRepository(CategorieAlbum::class);
+
+        $album = $albumRepo->find($idAlbum);
+        if (!$album) {
+            throw $this->createNotFoundException('Album non trouvé');
+        }
+
+        $photos = $album->getPhotos();
+        $categories = $categorieRepo->findAll();
+
+        // Albums regroupés par catégorie
+        $albumsByCategorie = [];
+        foreach ($categories as $categorie) {
+            $albumsByCategorie[$categorie->getIdCategorieAlbum()] = $albumRepo->findBy(['idCategorieAlbum' => $categorie]);
+        }
+
+        return $this->render('album/album.html.twig', [
+            'album' => $album,
+            'photos' => $photos,
+            'categories' => $categories,
+            'albumsByCategorie' => $albumsByCategorie,
+        ]);
+    }
+}
