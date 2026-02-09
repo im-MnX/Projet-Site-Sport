0c14745bff08666d0c8c314649350ae57e2b6913
Enzopaspris <enzo.martinet49460@gmail.com>
2026-02-09 10:21:31 +0100

    inscriptions/connexion

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -6,13 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class InscriptionController extends AbstractController
+class InscriptionController extends AbstractController
 {
     #[Route('/inscription', name: 'app_inscription')]
     public function index(): Response
     {
-        return $this->render('inscription/index.html.twig', [
-            'controller_name' => 'InscriptionController',
-        ]);
+        return $this->render('inscription/index.html.twig');
     }
 }
