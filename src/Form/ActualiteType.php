17b95a253c390c6215d26c7a9d18ae5d7b6a4cb4
flavien.leguerneve@gmail.com <etud@port-hp-probook650G8-5>
2026-02-10 10:25:13 +0100

    ajout admin actualité

---
+++ src/Form/ActualiteType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Actualite;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ActualiteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class, [
+                'label' => 'Titre de l\'actualité',
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('date', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date',
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('images', TextType::class, [
+                'label' => 'URL de l\'image',
+                'required' => false,
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'required' => false,
+                'attr' => ['class' => 'form-control', 'rows' => 5]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Actualite::class,
+        ]);
+    }
+}
