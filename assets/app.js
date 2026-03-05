1ea1a0ee309c9ac3d371447a552ffe5c4b73032b
etud user <etud@port-hp-probook650G8-15>
2026-03-05 12:42:37 +0100

    Correction erreurs console

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,2 @@
-import './stimulus_bootstrap.js';
-import 'bootstrap';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
